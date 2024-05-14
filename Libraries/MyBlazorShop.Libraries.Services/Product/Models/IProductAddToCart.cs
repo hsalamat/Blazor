089bfaa8ac2c9c8ce3f15099983273b512feace7
hsalamat <2hooman.salamat@gmail.com>
2024-05-14 16:28:56 -0400

    Other directives

--- Libraries/MyBlazorShop.Libraries.Services/Product/Models/IProductAddToCart.cs
+++ Libraries/MyBlazorShop.Libraries.Services/Product/Models/IProductAddToCart.cs
@@ -9,12 +9,17 @@ namespace MyBlazorShop.Libraries.Services.Product.Models
     /// <summary>
     /// The information needed in a product listing Razor component
     /// </summary>
-    public interface IProductListing
+    public interface IProductAddToCart
     {
         // An instance of the product
         ProductModel? Product { get; set; }
 
         // The quantity wishing to be added to the cart
         int? Quantity { get; set; }
+
+        /// <summary>
+        /// The method to add a product to cart
+        /// </summary>
+        void AddToCart();
     }
 }
